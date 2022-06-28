6d097bc6661d01e175ea83bbeedde6fe27d053db
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-06-28 03:50:05 +0300

    Prototurk template completed

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const _ = require('lodash')
+const plugin = require('tailwindcss/plugin')
+module.exports = {
+  mode : 'jit',
+  content: ["./**/*.html"],
+  darkMode: 'class', // or 'media'
+  theme: {
+    extend: {
+      colors:{
+        prototurk: '#32445a',
+        twitter: '#1da1f2',
+      },
+      spacing:{
+        '15' : '3.75rem'
+      },
+    },
+  },
+  plugins: [
+    plugin(function({ addUtilities, theme, e }) {
+      const calcUtilities = _.map(theme('spacing'), (value, key) => {
+        return {
+          [`.${e(`calc-h-full-${key}`)}`]: {
+            height: `calc(100% - (${value}))`
+          },
+          [`.${e(`calc-w-full-${key}`)}`]: {
+            width: `calc(100% - (${value}))`
+          }
+        }
+      })
+
+      addUtilities(calcUtilities, {
+        variants: ['responsive', 'hover'],
+      })
+    })
+  ],
+}
